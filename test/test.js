2cdab5a4594472f1314c9543e79bb29679710748
megastef <s.thies@bigdata-analyst.de>
2016-05-25 21:13:36 +0200

    test rewrite feature

--- test/test.js
+++ test/test.js
@@ -32,10 +32,40 @@ describe('Logsene log source', function () {
       var logger = new winston.Logger()
       logger.add(Logsene, {token: process.env.LOGSENE_TOKEN, source: 'mocha-test'})
       logger.info('Test', function (err, level, message, data) {
+        if (err) {
+          done(err)
+        }
         if (data.source === 'mocha-test') {
           done()
         } else {
           done(new Error('source not correct:' + data.source))
+        }
+      })
+    } catch (ex) {
+      console.log(ex.stack)
+      done(ex)
+    }
+  })
+})
+describe('Logsene rewrite hook', function () {
+  it('should have correct meta.ip field', function (done) {
+    try {
+      var winston = require('winston')
+      var Logsene = require('../lib/index.js')
+      var logger = new winston.Logger()
+      var serverIp = '10.0.0.12'
+      logger.add(Logsene, {
+        token: process.env.LOGSENE_TOKEN,
+        rewriter: function (level, msg, meta) {
+          meta.ip = serverIp
+          return meta
+        }
+      })
+      logger.info('Test', function (err, level, message, meta) {
+        if (meta.ip === serverIp) {
+          done()
+        } else {
+          done(new Error('rewrite field ip is not correct:' + meta.ip))
         }
       })
     } catch (ex) {
